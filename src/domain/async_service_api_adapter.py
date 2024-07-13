200d07f07b849c72863d059a38c04c653e94311e
yzLiu <pppoe4016@gmail.com>
2024-07-14 00:38:47 +0800

    feat(依照回饋修改 @check_response_code )
    依照回傳型態取用值 另可討論回傳值中的err是否可以拿掉 因為沒有被@check_response_code中try的error 捕捉到的 最後還是會被except捕捉 因此這個err就不用出現 也不用再在涵式內try except
    詳情可看check_response_code 與BFF中 _mentor_service.get_mentor_profile的邏輯

--- src/domain/async_service_api_adapter.py
+++ src/domain/async_service_api_adapter.py
@@ -5,42 +5,59 @@ from httpx import Response
 from starlette.responses import JSONResponse
 
 from src.domain.service_api import IServiceApi
+from src.infra.util.util import check_response_code
 
 
-class ServiceApiAdapter(IServiceApi):
+class AsyncServiceApiAdapter(IServiceApi):
+    @check_response_code('get', 200)
     async def simple_get(self, url: str, params: Dict = None, headers: Dict = None) -> (Optional[Dict[str, str]]):
         # 單純給service回的data
         async with httpx.AsyncClient() as client:
             response: Response = await client.get(url=url, headers=headers)
 
             dto: Dict = response.json()
-            return dto.get("data")  # 這裡應該是對應service的VO/responseDTO
 
+            return dto.get('data')  # 直接回傳數據內容
+
+    @check_response_code('get', 200)
     async def get(self, url: str, params: Dict = None, headers: Dict = None) -> (
             Optional[Dict[str, str]], Optional[str], Optional[str]):
+        dto: Dict = {}  # init with empty dict
+        msg: str = ''
+        err: str = ''
+
         async with httpx.AsyncClient() as client:
             response: Response = await client.get(url=url, headers=headers)
 
             dto: Dict = response.json()
-            server_res: JSONResponse = JSONResponse(content={
-                'msg': dto.get("msg"),
-                'data': dto.get("data")
-            })
+            msg: str = dto.get('msg')
 
-            return server_res  # 這裡應該是對應service的VO/responseDTO+msg
+            return dto.get('data'), msg, err  # 這裡應該是對應service的VO/responseDTO+msg
 
+    @check_response_code('get', 200)
     async def get_with_statuscode(self, url: str, params: Dict = None, headers: Dict = None) -> (
             Optional[Dict[str, str]], Optional[str], Optional[int], Optional[str]):
-        async with httpx.AsyncClient() as client:
-            response: Response = await client.get(url=url, headers=headers)
-            # 直接回傳服務的Response
-            return response.json()
+        dto: Dict = {}  # init with empty dict
+        msg: str = ''
+        err: str = ''
+        code: int = None
+        try:
+            async with httpx.AsyncClient() as client:
+                response: Response = await client.get(url=url, headers=headers)
+
+                dto: Dict = response.json()
+                msg: Optional[str] = dto.get('msg', None)
+                code: Optional[int] = dto.get('code', None)
+        except Exception as e:
+            err = e.__str__()
+
+        return dto.get('data'), msg, err, code  # 這裡應該是對應service的VO/responseDTO+msg
 
     async def simple_post(self, url: str, json: Dict, headers: Dict = None) -> (Optional[Dict[str, str]]):
         async with httpx.AsyncClient() as client:
             response: Response = await client.post(url=url, json=json, headers=headers)
             dto: Dict = response.json()
-            return dto.get("data")  # 這裡應該是對應service的VO/responseDTO
+            return dto.get('data')  # 這裡應該是對應service的VO/responseDTO
 
     async def post_data(self, url: str, byte_data: bytes, headers: Dict = None) -> (Optional[Dict[str, str]]):
         pass
@@ -51,8 +68,8 @@ class ServiceApiAdapter(IServiceApi):
             response: Response = await client.post(url=url, json=json, headers=headers)
             dto: Dict = response.json()
             server_res: JSONResponse = JSONResponse(content={
-                'msg': dto.get("msg"),
-                'data': dto.get("data")
+                'msg': dto.get('msg'),
+                'data': dto.get('data')
             })
 
             return server_res  # 這裡應該是對應service的VO/responseDTO+msg
@@ -62,13 +79,17 @@ class ServiceApiAdapter(IServiceApi):
         async with httpx.AsyncClient() as client:
             response: Response = await client.post(url=url, json=json, headers=headers)
             # 直接回傳服務的Response
-            return response.json()
+            result = response.json()
+            status_code = response.status_code
+            msg = result["msg"]
+            result = result["data"]
+            return result, msg, status_code, err
 
     async def simple_put(self, url: str, json: Dict = None, headers: Dict = None) -> (Optional[Dict[str, str]]):
         async with httpx.AsyncClient() as client:
             response: Response = await client.post(url=url, json=json, headers=headers)
             dto: Dict = response.json()
-            return dto.get("data")  # 這裡應該是對應service的VO/responseDTO
+            return dto.get('data')  # 這裡應該是對應service的VO/responseDTO
 
     async def put(self, url: str, json: Dict = None, headers: Dict = None) -> (
             Optional[Dict[str, str]], Optional[str], Optional[str]):
@@ -76,8 +97,8 @@ class ServiceApiAdapter(IServiceApi):
             response: Response = await client.put(url=url, json=json, headers=headers)
             dto: Dict = response.json()
             server_res: JSONResponse = JSONResponse(content={
-                'msg': dto.get("msg"),
-                'data': dto.get("data")
+                'msg': dto.get('msg'),
+                'data': dto.get('data')
             })
 
             return server_res  # 這裡應該是對應service的VO/responseDTO+msg
@@ -93,7 +114,7 @@ class ServiceApiAdapter(IServiceApi):
         async with httpx.AsyncClient() as client:
             response: Response = await client.delete(url=url, headers=headers)
             dto: Dict = response.json()
-            return dto.get("data")  # 這裡應該是對應service的VO/responseDTO
+            return dto.get('data')  # 這裡應該是對應service的VO/responseDTO
 
     async def delete(self, url: str, params: Dict = None, headers: Dict = None) -> (
             Optional[Dict[str, str]], Optional[str], Optional[str]):
@@ -101,8 +122,8 @@ class ServiceApiAdapter(IServiceApi):
             response: Response = await client.delete(url=url, headers=headers)
             dto: Dict = response.json()
             server_res: JSONResponse = JSONResponse(content={
-                'msg': dto.get("msg"),
-                'data': dto.get("data")
+                'msg': dto.get('msg'),
+                'data': dto.get('data')
             })
 
             return server_res  # 這裡應該是對應service的VO/responseDTO+msg
--- src/infra/util/util.py
+++ src/infra/util/util.py
@@ -1,5 +1,6 @@
 import functools
 import time
+from typing import Dict
 
 from starlette import status
 
@@ -28,8 +29,6 @@ def get_serial_num(cache: ICache, user_id: str):
     return user[SERIAL_KEY]
 
 
-
-
 def check_response_code(method: str, expected_code: int = 200):
     def decorator_check_response_code(func):
         @functools.wraps(func)
@@ -39,37 +38,51 @@ def check_response_code(method: str, expected_code: int = 200):
             params = kwargs.get('params', None)
             body = kwargs.get('json', None)
             headers = kwargs.get('headers', None)
+            err: str = ''
             try:
-                response = await func(*args, **kwargs)
-                status_code = int(response.get('code', None)) #回傳為res_sucess/ res_error型態
-                if status_code < 400 or response.status_code == expected_code:
-                    return response
+                service_api_res = await func(*args, **kwargs)
+                # 代表是simple系列
+                if type(service_api_res) is not tuple:
+                    return service_api_res
+                data: Dict = service_api_res[0]
+                msg: str = service_api_res[1]
+                err: str = service_api_res[2]
+                #ex: 40400 取至404
+                status_code: int = int(service_api_res[3][:3]) if len(service_api_res) >= 3 else None
+                if status_code is not None and status_code < 400 or status_code == expected_code:
+                    return service_api_res
 
-                response_json = response.json()
-                msg = response_json["msg"] if "msg" in response_json else response.reason
-                data = response_json["data"] if "data" in response_json else None
                 log.error(
                     f"service request fail, [%s]: %s, body:%s, params:%s, headers:%s, status_code:%s, msg:%s, \n response:%s",
-                    method, url, body, params, headers, status_code, msg, response)
+                    method, url, body, params, headers, status_code, msg, service_api_res)
+                if status_code is not None:
 
-                if status_code == status.HTTP_400_BAD_REQUEST:
-                    raise ClientException(msg=msg, data=data)
+                    if status_code == status.HTTP_400_BAD_REQUEST:
+                        raise ClientException(msg=msg, data=data)
 
-                if status_code == status.HTTP_401_UNAUTHORIZED:
-                    raise UnauthorizedException(msg=msg, data=data)
+                    if status_code == status.HTTP_401_UNAUTHORIZED:
+                        raise UnauthorizedException(msg=msg, data=data)
 
-                if status_code == status.HTTP_403_FORBIDDEN:
-                    raise ForbiddenException(msg=msg, data=data)
+                    if status_code == status.HTTP_403_FORBIDDEN:
+                        raise ForbiddenException(msg=msg, data=data)
 
-                if status_code == status.HTTP_404_NOT_FOUND:
-                    raise NotFoundException(msg=msg, data=data)
+                    if status_code == status.HTTP_404_NOT_FOUND:
+                        raise NotFoundException(msg=msg, data=data)
 
-                if status_code == status.HTTP_406_NOT_ACCEPTABLE:
-                    raise NotAcceptableException(msg=msg, data=data)
+                    if status_code == status.HTTP_406_NOT_ACCEPTABLE:
+                        raise NotAcceptableException(msg=msg, data=data)
 
                 raise ServerException(msg=msg, data=data)
-            except Exception as e: #這邊捕捉非Server response 狀態錯誤(有正常回傳resp)的情況
-                err_msg: str = f"unhandled {function_name} request error, {url}, {params}, {headers}"
+            except Exception as e:  # 這邊捕捉Server side err
+                err = e.__str__()
+
+                err_msg: str = (
+                    f"unhandled {function_name} request error,\n"
+                    f"url:{url}, \n"
+                    f"params: {params}\n, "
+                    f"headers: {headers}\n,"
+                    f"error detail: {err} \n,"
+                    )
                 log.error(err_msg)
                 raise ServerException(msg=err_msg)
 
