40ff5aeede1f7bc198de7ef278ae331eaa352af0
yzLiu <pppoe4016@gmail.com>
2024-11-24 13:48:26 +0800

    feat(revise response to make it more flexible)

--- src/app/template/service_response.py
+++ src/app/template/service_response.py
@@ -10,13 +10,23 @@ class ServiceApiResponse(ClientResponse):
     @staticmethod
     def parse(response: httpx.Response = None) -> 'ServiceApiResponse':
         if response:
+            parsed_data = response.json()
+            raw_data = parsed_data.get('data', {})
+            # Ensure `data` is parsed properly
+            if isinstance(raw_data, str):
+                try:
+                    data = json.loads(raw_data)
+                except json.JSONDecodeError:
+                    data = raw_data  # only a string
+            else:
+                data = raw_data
             return ServiceApiResponse(
                 status_code=response.status_code,
                 headers=response.headers,
-                res_json=response.json(),
+                res_json=parsed_data,
                 res_content=response.content,
                 res_text=response.text,
-                data=json.loads(response.json().get('data', {}))
+                data=data
             )
 
         return None
